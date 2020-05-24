e6ca95443fefe9aa83f89c3b6e679e9e11140572
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-05-24 15:14:42 -0500

    Create knex migrations & db schema -> Generate seed data

---
+++ db/migrations/20200524140122_master_schema.js
@@ -0,0 +1,42 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', tbl => {
+      tbl.increments();
+
+      tbl.string('username', 25)
+        .notNullable()
+        .unique();
+
+      tbl.string('password', 128)
+        .notNullable();
+
+      tbl.integer('phone_number')
+        .notNullable();
+    })
+    .createTable('plants', tbl => {
+      tbl.increments();
+
+      tbl.string('nickname', 128)
+        .notNullable();
+
+      tbl.string('species', 128);
+
+      tbl.string('h2o_frequency', 25)
+        .notNullable();
+
+      tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('plants');
+};
