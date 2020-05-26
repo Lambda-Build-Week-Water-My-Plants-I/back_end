bbd919d4b9813d012e354b11fef87f8fd1d38c1c
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-05-26 13:26:24 -0500

    FACEPALM . . . I forgot to even install PG.

--- knexfile.js
+++ knexfile.js
@@ -26,10 +26,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './db/migrations'
     },
